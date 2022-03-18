e8d0771133116619501d20194d565f559e17b11a
bang-js <95086336+bang-js@users.noreply.github.com>
2022-03-18 22:59:30 +0900

    Update sigma_algebra.py
    재귀함수 이용하여 2->n개로 확장

--- sigma_algebra.py
+++ sigma_algebra.py
@@ -42,18 +42,71 @@ print("empty:",empty)
 # a의 원소가 2개인 제한적 케이스
 ##
 # zip 적용 후 list 형태로 변환
-zipped = list(zip(a,a_comp,empty))  # [(),(),...]
-zipped_lst = [] 
-for i in zipped :                   # [[],[],...]
-    zipped_lst.append(list(i))
-print("zipped_lst", zipped_lst)
-for z0 in zipped_lst[0] :
-    for z1 in zipped_lst[1] :
-        # print(z0,z1)
-        # print(type(z0),type(z1))
-        if not(set.union(z0,z1) in sigma_algebra)  :  # 중복 시 포함안함
-            sigma_algebra.append(set.union(z0,z1))
-        if not(set.intersection(z0,z1) in sigma_algebra)  :
-            sigma_algebra.append(set.intersection(z0,z1))
+
+# zipped = list(zip(a,a_comp,empty))  # [(),(),...]
+# zipped_lst = [] 
+# for i in zipped :                   # [[],[],...]
+#     zipped_lst.append(list(i))
+# print("zipped_lst", zipped_lst)
+# for z0 in zipped_lst[0] :
+#     for z1 in zipped_lst[1] :
+#         # print(z0,z1)
+#         # print(type(z0),type(z1))
+#         if not(set.union(z0,z1) in sigma_algebra)  :  # 중복 시 포함안함
+#             sigma_algebra.append(set.union(z0,z1))
+#         if not(set.intersection(z0,z1) in sigma_algebra)  :
+#             sigma_algebra.append(set.intersection(z0,z1))
+# print("sigma_algebra:",sigma_algebra)
+# print("length:",len(sigma_algebra))
+
+
+###
+# 이제 연쇄연산을 이용하여 n개의 zipped에 대해 각 zipped에서 하나씩 추출해서 and와 or 연산을 해준 결과를 출력해주면 됨
+# 재귀함수 사용
+###
+
+# 임의의 집합족에서 중복 원소를 지우는 함수
+def era(family):
+    unique = []
+    for i in range(len(family)-1) :
+        n = 0
+        for j in range(i+1, len(family)) :
+            if family[i] == family[j] :
+                n += 1
+        if n == 0 :
+            unique.append(family[i])
+    unique.append(family[len(family)-1])
+    return unique
+
+# 재귀함수를 사용하여 A1~An에 대해 연쇄연산
+# 그림으로 표현하면 
+# A1----U/I-----A2----U/I--...--An      
+# A1^c          A2^c            An^c
+# null          null            null
+
+# 먼저 An-1(+An-1^c,null)과 An(+An^c,null)에 대해 연쇄연산(합,교)(3x3=9번) 
+# -> 그 결과를 중복제거한 뒤 temp에 저장, temp는 일종의 n-1과 n의 결합체
+# -> temp와 An-2에 대해 연쇄연산 ((9-k)x3번 연산, k는 중복된 원소 개수) 
+# -> 이를 재귀적으로 반복
+def FN(n):
+    while n >= 1 :
+        print("n-1, n번 elements of zipped_lst : ",zipped_lst[n-1], zipped_lst[n])
+        temp =[]
+        for z0 in zipped_lst[n-1] :
+            for z1 in zipped_lst[n] :
+                if not(set.union(z0,z1) in sigma_algebra)  :  # 중복 시 포함안함
+                    sigma_algebra.append(set.union(z0,z1))
+                temp.append(set.union(z0,z1))                  # 합집합 연산한 결과를 temp에 저장
+                if not(set.intersection(z0,z1) in sigma_algebra)  :
+                    sigma_algebra.append(set.intersection(z0,z1))
+                temp.append(set.intersection(z0,z1))
+        del zipped_lst[n]
+        del zipped_lst[n-1]
+        temp = era(temp)        # temp 내 중복되는 원소 제거 (계산 효율성)
+        zipped_lst.append(temp)
+        # print("temp:",temp, "\n","zipped_lst:",zipped_lst)
+        return FN(n-1)
+
+FN(len(zipped_lst)-1)
 print("sigma_algebra:",sigma_algebra)
 print("length:",len(sigma_algebra))
